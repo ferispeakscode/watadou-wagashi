31397ee012bc23a4cf59ee96a9e6a2c3e42c835a
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-09-22 22:40:21 -0400

    Added images, wagashi information

---
+++ shared/wagashi.js
@@ -0,0 +1,42 @@
+export const WAGASHI = [
+    {
+        id: 0,
+        name: "Mejiro",
+        image: "",
+        artisan: "Hanaoka Nao",
+        featured: false,
+        description: "A \"mejiro\", Japanese white-eye, said to have a golden soprano voice. Eat it with the beak pointed towards the direction of good luck for good luck."
+    },
+    {
+        id: 1,
+        name: "Usuzumi-zakura",
+        image: "/assets/images/usuzumi-zakura.jpg",
+        artisan: "Takatsuki Tsubaki",
+        featured: false,
+        description: "Usuzumi-zakura are a special type of sakura (cherry blossom). As buds, they are a light red, but turn white upon full bloom. Finally, they look like they've been tinted in ink when they finally fall. Unlike the typical sakura you see, usuzumi-zakura are like marriage, which only happens once in a lifetime, and ought to be paired with something just as unique and gorgous."
+    },
+    {
+        id: 2,
+        name: "Hazakura",
+        image: "/assets/images/hazakura.jpg",
+        artisan: "Hanaoka Nao",
+        featured: true,
+        description: "Sakura (cherry blossom) are indeed most beautiful when they are pink and in full bloom. However, the spring winds will blow soon after and those beautiful petals will fall. As if they have been waiting for that moment, the leaves of the sakura trees grow strong and thick, and before long the season unfolds in greenery. Like so, hazakura carries the good wishes of a lasting, happy marriage. True happiness comes from the daily toils of life."
+    },
+    {
+        id: 3,
+        name: "Shingetsu",
+        image: "/assets/images/shingetsu.jpg",
+        artisan: "Hanaoka Nao",
+        featured: false,
+        description: "Nights with a moon, especially full moons, are particularly beautiful. Sometimes, a chestnut is added inside a yokan to symbolize the golden moon in the night sky. However, nights without a moon are even more beautiful as they are lit by the light of all the stars in the sky. The flowers will smell more fragrant, and we will be able to better-see the true face of the night."
+    },
+    {
+        id: 4,
+        name: "Shirabuji",
+        image: "/assets/images/shirabuji.jpg",
+        artisan: "Hanaoka Nao",
+        featured: false,
+        description: "Shirabuji is a high-grade wagashi depicting a wisteria flower growing on the wagashi. Wisteria symbolize the continuation of generations further."
+    }
+];
