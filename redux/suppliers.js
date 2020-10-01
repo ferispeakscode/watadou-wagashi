4a1820a8bd1f59cca3fce6a5f7f8aabfcc33368b
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-01 00:11:23 -0400

    Updated suppliers in redux

--- redux/suppliers.js
+++ redux/suppliers.js
@@ -16,30 +16,3 @@ export const suppliers = (state = { isLoading: true,
             return state;
     }
 };
-
-
-
-
-
-
-
-
-import * as ActionTypes from './ActionTypes';
-
-export const partners = (state = { isLoading: true,
-                                    errMess: null,
-                                    partners: []}, action) => {
-    switch (action.type) {
-        case ActionTypes.ADD_PARTNERS:
-            return {...state, isLoading: false, errMess: null, partners: action.payload};
-
-        case ActionTypes.PARTNERS_LOADING:
-            return {...state, isLoading: true, errMess: null, partners: []}
-
-        case ActionTypes.PARTNERS_FAILED:
-            return {...state, isLoading: false, errMess: action.payload};
-
-        default:
-          return state;
-    }
-};
