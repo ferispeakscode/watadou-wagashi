33d6736e04449562fb9ee36c5a506a5a08b48ba3
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-01 23:02:49 -0400

    Configured app to persist

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,23 +1,33 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { comments } from './comments';
-import { promotions } from './promotions';
-import { favorites } from './favorites';
 import { wagashi } from './wagashi';
+import { comments } from './comments';
+import { favorites } from './favorites';
+import { promotions } from './promotions';
 import { suppliers } from './suppliers';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+};
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
-            comments,
-            promotions,
-            favorites,
+        persistCombineReducers(config, {
             wagashi,
+            comments,
+            favorites,
+            promotions,
             suppliers
         }),
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store);
+
+    return { persistor, store };
 }
