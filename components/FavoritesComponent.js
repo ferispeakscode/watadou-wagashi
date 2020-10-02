e0edda6a742106a7f9b06154fd26bd3d80bfe83b
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-01 22:07:11 -0400

    Updated content for Favorites Component

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,13 +1,93 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Alert, View, Text, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+// import { deleteFavorite } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
+
+
+const mapStateToProps = state => {
+    return {
+        wagashi: state.wagashi,
+        favorites: state.favorites
+    };
+};
+
+const mapDispatchToProps = {
+    deleteFavorite: wagashiId => deleteFavorite(wagashiId)
+};
 
 class Favorites extends Component {
 
+    static navigationOptions = {
+        title: 'My Favorite Wagashi'
+    }
+
     render() {
+        const { navigate } = this.props.navigation;
+        const renderFavoriteItem = ({item}) => {
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => {
+                        Alert.alert(
+                            'Delete Favorite?',
+                            'Are you sure you wish to delete this wagashi, + ' + item.name + ' from favorites?',
+                            [
+                                {
+                                    text: 'Cancel',
+                                    style: 'cancel',
+                                    onPress: () => console.log(item.name + 'not deleted.'),
+                                },
+                                {
+                                    text: "Yes",
+                                    onPress: () => this.props.deleteFavorite(item.id)
+                                }
+                            ],
+                            { cancelable: false }
+                        )
+                    }
+                }
+            ];
+
+            return(
+                <Swipeout right={rightButton} autoClose={true}>
+                    <Animatable.View animation='fadeInRightBig' duration={2000}>
+                        <ListItem 
+                            title={item.name}
+                            subtitle={item.description}
+                            leftAvatar={{source: {uri: baseUrl + item.image}}}
+                            onPress={() => navigate('WagashiDetail', { wagashiId: item.id })}
+                        />
+                    </Animatable.View>
+                </Swipeout>
+            );
+        };
+
+        if (this.props.wagashi.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.wagashi.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.wagashi.errMess}</Text>
+                </View>
+            );
+        }
         return(
-            <View />
+            <FlatList
+                data={thisprops.wagashi.wagashi.filter(
+                    w => this.props.favorites.includes(w.id)
+                )}
+                renderItem={renderFavoriteItem}
+                keyExtractor={item => item.id.toString()}
+            />
         );
     }
 }
 
-export default Favorites;
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
