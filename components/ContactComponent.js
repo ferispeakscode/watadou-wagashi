e9213e427cecaed4d7cb19de9ac4bf6f864deb63
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-03 00:19:11 -0400

    Added email button to Contact page

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
 
@@ -8,15 +9,34 @@ class Contact extends Component {
         title: 'Contact Us'
     }
 
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['shokunin@wagashi.co'],
+            subject: 'Inquiry Regarding Wagashi',
+            body: 'To whom it may concern:'
+        });
+    }
+
     render() {
         return(
             <ScrollView>
                 <Card title="Contact Information" wrapperStyle={{margin: 20}}>
-                    <Text>1 Nucamp Way</Text>
-                    <Text>Seattle, WA 98001</Text>
+                    <Text>9 Wagashi Lane</Text>
+                    <Text>New York, NY 10001</Text>
                     <Text style={{marginBottom: 10}}>U.S.A.</Text>
-                    <Text>Phone: 1-206-555-1234</Text>
-                    <Text>Email: campsites@nucamp.co</Text>
+                    <Text>Phone: 1-202-314-1512</Text>
+                    <Text>Email: shokunin@wagashi.co</Text>
+                    <Button
+                        title="Send Email"
+                        buttonStyle={{backgroundColor: '#AE323B', margin: 40}}
+                        icon={<Icon
+                            name='envelope-o'
+                            type='font-awesome'
+                            color='#EAE8ED'
+                            iconStyle={{marginRight: 10}}
+                        />}
+                        onPress={() => this.sendMail()}
+                    />
                 </Card>
             </ScrollView>
         );
