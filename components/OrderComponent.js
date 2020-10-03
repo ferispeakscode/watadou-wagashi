df57344702030b81b5bf08aaea771ca509cd52ec
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-10-03 00:31:20 -0400

    Added notification functionality to order component

--- components/OrderComponent.js
+++ components/OrderComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Order extends Component {
 
@@ -33,7 +35,10 @@ class Order extends Component {
                 },
                 {
                     text: "Submit",
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.type, this.state.number, this.state.date);
+                        this.resetForm();
+                    }
                 }
             ],
             { cancelable: false }
@@ -48,6 +53,28 @@ class Order extends Component {
             date: '',
             gift: false,
         });
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notification');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(type, number, date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Wagashi Order',
+                body: 'You ordered ' + number + " " + type + ' for pickup on ' + date + "."
+            });
+        }
     }
 
     render() {
